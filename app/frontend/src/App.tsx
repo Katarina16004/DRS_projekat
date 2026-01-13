a7645e1a559f559b179efbff8e659b68daccda76
Bojana Glumac <bojanaglumac21@gmail.com>
2026-01-14 00:50:42 +0100

    Rute za Login i Profile page

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,44 +1,44 @@
 import {
   Routes,
   Route,
-  Navigate,
+  Navigate
 } from "react-router-dom";
+
 import RegistrationPage from "./pages/auth/RegistrationPage";
+import LoginPage from "./pages/auth/LoginPage";
 import NotFoundStranica from "./pages/not_found/NotFoundPage";
 import { Toaster } from "react-hot-toast";
+import ProfilePage from "./pages/profile/ProfilePage";
 
 function App() {
   return (
     <>
-    <Toaster
-      position="top-center"
-      reverseOrder={false}
-      toastOptions={{
-        duration: 3000,
-        style: {
-          padding: '16px',
-          color: '#fff',
-          borderRadius: '8px',
-          fontWeight: '600',
-        },
-        success: {
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        toastOptions={{
+          duration: 3000,
           style: {
-            background: 'green', 
+            padding: "16px",
+            color: "#fff",
+            borderRadius: "8px",
+            fontWeight: "600",
           },
-        },
-        error: {
-          style: {
-            background: 'red', 
-          },
-        },
-      }}
-    />
-    <Routes>
-      <Route path="/register" element={<RegistrationPage/>} />
-      <Route path="/404" element={<NotFoundStranica />} />
-      <Route path="/" element={<Navigate to="/register" replace />} />
-      <Route path="*" element={<Navigate to="/404" replace />} />
-    </Routes>
+          success: { style: { background: "green" } },
+          error: { style: { background: "red" } },
+        }}
+      />
+
+
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegistrationPage />} />
+        <Route path="/profileInfo" element={<ProfilePage />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/404" element={<NotFoundStranica />} />
+        <Route path="*" element={<Navigate to="/404" replace />} />
+      </Routes>
+
     </>
   );
 }
