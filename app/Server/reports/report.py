d224855aea304a8266ae90046ab875472785d8dd
Katarina16004 <veljkovickatarina56@gmail.com>
2026-02-08 21:28:49 +0100

    fix gresaka na backu

--- app/Server/reports/report.py
+++ app/Server/reports/report.py
@@ -14,10 +14,10 @@ def generate_game_report(game):
 
     pdf.set_font("Arial", size=12)
 
-    pdf.cell(200, 10, f"Game ID: {game["ID_Game"]}", ln=True)
-    pdf.cell(200, 10, f"Player ID: {game["ID_Player"]}", ln=True)
-    pdf.cell(200, 10, f"Quiz ID: {game["ID_Quiz"]}", ln=True)
-    pdf.cell(200, 10, f"Score: {game["Score"]}", ln=True)
+    pdf.cell(200, 10, f"Game ID: {game['ID_Game']}", ln=True)
+    pdf.cell(200, 10, f"Player ID: {game['ID_Player']}", ln=True)
+    pdf.cell(200, 10, f"Quiz ID: {game['ID_Quiz']}", ln=True)
+    pdf.cell(200, 10, f"Score: {game['Score']}", ln=True)
 
     pdf.ln(5)
 
@@ -28,7 +28,7 @@ def generate_game_report(game):
     pdf.cell(200, 10, f"Date: {date_str}", ln=True)
     pdf.cell(200, 10, f"Time: {time_str}", ln=True)
 
-    filename = f"{game["ID_Player"]}_{date_str}_{time_str}_report.pdf"
+    filename = f"{game['ID_Player']}_{date_str}_{time_str}_report.pdf"
 
     pdf.output(filename)
 
